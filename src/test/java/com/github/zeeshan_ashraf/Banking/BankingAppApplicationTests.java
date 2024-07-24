b553bb30bd82c5a806c30e148ec6d0a99a006c5d
Zeeshan Ashraf <mashraf@tripadvisor.com>
2024-07-24 18:32:09 +0530

    setup github repo

---
+++ src/test/java/com/github/zeeshan_ashraf/Banking/BankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zeeshan_ashraf.Banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
